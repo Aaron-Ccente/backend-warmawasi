cb49717376b923adf2e8d29fd6aec5923ff78b8a
José <clasesvirtualez96@gmail.com>
2025-11-29 23:12:06 -0500

    configuracion exitosa

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/prueba', function () {
+    return response()->json([
+        'mensaje' => '¡Conexión exitosa desde Laravel!',
+        'status' => 'ok'
+    ]);
+});
